3cb6f3c8703d2fca085a7a70fb1c324d891e0a9d
JNelson <nelson.jimmy@gmail.com>
2018-03-26 19:11:30 -0600

    resolving hound errors

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -1,28 +1,36 @@
 require 'rails_helper'
 
-describe "User sees a specific job" do
-  scenario "a user sees a job for a specific company" do
-    company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+describe 'User sees a specific job' do
+  scenario 'a user sees a job for a specific company' do
+    company = Company.create!(name: 'ESPN')
+    job = company.jobs.create!(
+      title: 'Developer',
+      level_of_interest: 70,
+      city: 'Denver'
+    )
 
     visit company_job_path(company, job)
 
-    expect(page).to have_content("ESPN")
-    expect(page).to have_content("Developer")
-    expect(page).to have_content("70")
+    expect(page).to have_content('ESPN')
+    expect(page).to have_content('Developer')
+    expect(page).to have_content('70')
   end
 end
 
-describe "User clicks on a specific job" do
-  scenario "a user sees a job for a specific company when they click" do
-    company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+describe 'User clicks on a specific job' do
+  scenario 'a user sees a job for a specific company when they click' do
+    company = Company.create!(name: 'ESPN')
+    company.jobs.create!(
+      title: 'Developer',
+      level_of_interest: 70,
+      city: 'Denver'
+    )
 
     visit company_path(company)
     click_link 'Developer'
 
-    expect(page).to have_content("ESPN")
-    expect(page).to have_content("Developer")
-    expect(page).to have_content("70")
+    expect(page).to have_content('ESPN')
+    expect(page).to have_content('Developer')
+    expect(page).to have_content('70')
   end
 end
