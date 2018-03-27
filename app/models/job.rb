7da65fe23a701128e61531935deeb8fa645dd704
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-03-27 00:02:34 -0600

    Adds dependency for job removal destroying comments it owns

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,5 @@
 class Job < ApplicationRecord
   validates :title, :level_of_interest, :city, presence: true
   belongs_to :company
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
