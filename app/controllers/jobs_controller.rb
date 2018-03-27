139b5a913c96745b6034d0f402ba1a15aca1593b
JNelson <nelson.jimmy@gmail.com>
2018-03-26 18:51:35 -0600

    can't figure out why capybara won't pass this test about not updating a job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -33,7 +33,7 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
     @company = @job.company
     @job.update(job_params)
-    if @job.save
+    if @job.update
       flash.notice = "Job '#{@job.title}' was updated."
       redirect_to company_job_path(@company, @job)
     else
