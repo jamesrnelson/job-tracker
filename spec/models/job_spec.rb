3667ca51ac3b5502a12dfb23f062857aa0d04eea
JNelson <nelson.jimmy@gmail.com>
2018-03-27 06:10:21 -0600

    new tests for job depending on category

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,43 +1,67 @@
 require 'rails_helper'
 
 describe Job do
-  describe "validations" do
-    context "invalid attributes" do
-      it "is invalid without a title" do
-        job = Job.new(level_of_interest: 80, description: "Wahoo", city: "Denver")
+  describe 'validations' do
+    context 'invalid attributes' do
+      it 'is invalid without a title' do
+        job = Job.new(level_of_interest: 80, description: 'Wahoo', city: 'Denver')
         expect(job).to be_invalid
       end
 
-      it "is invalid without a level of interest" do
-        job = Job.new(title: "Developer", description: "Wahoo", city: "Denver")
+      it 'is invalid without a level of interest' do
+        job = Job.new(title: 'Developer', description: 'Wahoo', city: 'Denver')
         expect(job).to be_invalid
       end
 
-      it "is invalid without a city" do
-        job = Job.new(title: "Developer", description: "Wahoo", level_of_interest: 80)
+      it 'is invalid without a city' do
+        job = Job.new(title: 'Developer', description: 'Wahoo', level_of_interest: 80)
+        expect(job).to be_invalid
+      end
+
+      it 'is invalid without a category' do
+        job = Job.new(
+          title: 'Developer',
+          description: 'Wahoo',
+          level_of_interest: 80,
+          city: 'Denver')
+
         expect(job).to be_invalid
       end
     end
 
-    context "valid attributes" do
-      it "is valid with a title, level of interest, and company" do
-        company = Company.new(name: "Turing")
-        job = Job.new(title: "Developer", level_of_interest: 40, city: "Denver", company: company)
+    context 'valid attributes' do
+      it 'is valid with a title, level of interest, company, and caetegory' do
+        company = Company.new(name: 'Turing')
+        category = Category.new(title: 'Web Development')
+        job = Job.new(
+          title: 'Developer',
+          level_of_interest: 40,
+          city: 'Denver',
+          company: company,
+          category: category
+        )
         expect(job).to be_valid
       end
     end
   end
 
-  describe "relationships" do
-    it "belongs to a company" do
-      job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
+  describe 'relationships' do
+    it 'belongs to a company' do
+      job = Job.new(title: 'Software', level_of_interest: 70, description: 'Wahooo')
       expect(job).to respond_to(:company)
     end
   end
 
-  describe "relationships" do
-    it "has many comments" do
-      job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
+  describe 'relationships' do
+    it 'belongs to a category' do
+      job = Job.new(title: 'Software', level_of_interest: 70, description: 'Wahooo')
+      expect(job).to respond_to(:company)
+    end
+  end
+
+  describe 'relationships' do
+    it 'has many comments' do
+      job = Job.new(title: 'Software', level_of_interest: 70, description: 'Wahooo')
 
       expect(job).to respond_to(:comments)
     end
