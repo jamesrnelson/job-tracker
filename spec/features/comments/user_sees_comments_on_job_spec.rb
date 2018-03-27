f044428309f63361120887529712046fe146a09c
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-03-27 00:24:29 -0600

    Creates working test for comment output on job view

--- spec/features/comments/user_sees_comments_on_job_spec.rb
+++ spec/features/comments/user_sees_comments_on_job_spec.rb
@@ -4,11 +4,11 @@ describe "User views a job page" do
   scenario "a user sees a comment section" do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", company: company)
-    comment = Comment.new(body: 'This is the body', job: job)
-    comment2 = Comment.new(body: 'Esta es una body', job: job)
+    comment = job.comments.create(body: 'This is the body')
+    comment2 = job.comments.create(body: 'Esta es una body')
 
     visit company_job_path(company, job)
-    binding.pry
+
     expect(page).to have_content('Comments:')
     expect(page).to have_content(comment.body)
     expect(page).to have_content(comment2.body)
