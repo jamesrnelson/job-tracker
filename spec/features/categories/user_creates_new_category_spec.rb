938578a4a2a00aa7505d23c9cc8e8b2dcffb7519
JNelson <nelson.jimmy@gmail.com>
2018-03-26 22:00:08 -0600

    implemented passing category create tests

--- spec/features/categories/user_creates_new_category_spec.rb
+++ spec/features/categories/user_creates_new_category_spec.rb
@@ -2,35 +2,27 @@ require 'rails_helper'
 
 describe 'User creates a new category' do
   scenario 'a user can create a new category' do
-    category = Category.create!(title: 'Web development')
-    visit new_category_path(category)
+    visit new_category_path
 
     fill_in 'category[title]', with: 'Database Administration'
 
-    click_button 'Create New Category'
+    click_button 'Create'
 
-    expect(current_path).to eq("/categories/#{category.id}")
+    expect(current_path).to eq("/categories/#{Category.last.id}")
     expect(page).to have_content('Database Administration')
   end
 end
 
 describe 'User creates a new category from category listing' do
   scenario 'a user clicks the link to go to form' do
-    category = Category.create!(title: 'finance')
-    visit company_path(company)
-    click_link 'Create a New Job'
+    visit categories_path
+    click_link 'Create a New Category'
 
-    fill_in 'job[title]', with: 'Developer'
-    fill_in 'job[description]', with: 'So fun!'
-    fill_in 'job[level_of_interest]', with: 80
-    fill_in 'job[city]', with: 'Denver'
+    fill_in 'category[title]', with: 'Database Administration'
 
     click_button 'Create'
 
-    expect(current_path).to eq("/companies/#{company.id}/jobs/#{Job.last.id}")
-    expect(page).to have_content('ESPN')
-    expect(page).to have_content('Developer')
-    expect(page).to have_content('80')
-    expect(page).to have_content('Denver')
+    expect(current_path).to eq("/categories/#{Category.last.id}")
+    expect(page).to have_content('Database Administration')
   end
 end
