c6a6594d67a0b4604e851ef46a6e9f19de9c7460
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-03-28 11:16:58 -0600

    single quotes instead of double

--- spec/features/jobs/user_can_see_all_jobs_on_jobs_index_spec.rb
+++ spec/features/jobs/user_can_see_all_jobs_on_jobs_index_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe 'User sees all jobs' do
   scenario 'a user sees all jobs on jobs#index' do
-    company = Company.create!(name: "ESPN")
+    company = Company.create!(name: 'ESPN')
     category = Category.create!(title: 'Web Development')
-    company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category: category)
-    company.jobs.create!(title: "Getter who gets", level_of_interest: 100, city: "Danver", category: category)
+    company.jobs.create!(title: 'Developer', level_of_interest: 70, city: 'Denver', category: category)
+    company.jobs.create!(title: 'Getter who gets', level_of_interest: 100, city: 'Danver', category: category)
 
 
     visit jobs_path
