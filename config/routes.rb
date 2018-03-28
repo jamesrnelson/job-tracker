c23dc3c8108b5c2a14bc9fb4c7a1fde8cf35809b
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-03-28 10:33:13 -0600

    Adds shallow routing for job-comments

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   end
 
   resources :jobs do
-    resources :comments, only: :create
+    resources :comments, shallow: true
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
