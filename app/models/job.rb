6f0ceba5e33a4aa50d33869ba06b9f99651747da
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-03-27 19:31:49 -0600

    Adds comment order method to job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,8 @@ class Job < ApplicationRecord
   validates :title, :level_of_interest, :city, presence: true
   belongs_to :company
   has_many :comments, dependent: :destroy
+
+  def comment_order
+    self.comments.reverse
+  end
 end
