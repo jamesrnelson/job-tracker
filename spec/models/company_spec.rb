78b3584a1e850f19c95ae4f03a80cfa9530121de
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-03-28 09:34:11 -0600

    Adds dependency deletion test for company -> job

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -36,4 +36,22 @@ describe Company do
       expect(company).to respond_to(:contacts)
     end
   end
+
+  describe "dependencies" do
+    it 'deletes jobs it owns when deleted' do
+      company = Company.create(name: 'Turing')
+      category = Category.create(title: 'Web Development')
+      job = Job.create(
+        title: 'Developer',
+        level_of_interest: 40,
+        city: 'Denver',
+        company: company,
+        category: category
+      )
+
+      company.destroy
+
+      expect(Job.count).to eq(0)
+    end
+  end
 end
