b31f1f39b4c3fdfd63e667fd065811b8fa822007
JNelson <nelson.jimmy@gmail.com>
2018-03-28 10:02:30 -0600

    Shows top companies by average level of interest

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,5 +5,6 @@ class DashboardController < ApplicationController
     @job25_50 = Job.count_between_25_and_50
     @job0_25 = Job.count_less_than_25
     @distinct_cities = Job.distinct_cities
+    @top_three_companies = Company.top_three_companies
   end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,14 @@
 class Company < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :jobs, dependent: :destroy
+
+  def self.top_three_companies
+    joins(:jobs)
+    .group(:id)
+    .order('avg(level_of_interest) desc')[0..2]
+  end
+
+  def average_level_of_interest
+    jobs.average(:level_of_interest)
+  end
 end
